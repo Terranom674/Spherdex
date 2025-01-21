f87dad87e81063071225820ad5572595e098853d
Terranom674 <thomas_dannenberg@web.de>
2025-01-21 21:06:07 +0100

    Delete members_management directory

--- members_management/mitgliederverwaltung/doctype/mitglied/mitglied.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Thomas Dannenberg and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Mitglied", {
-// 	refresh(frm) {
-
-// 	},
-// });
