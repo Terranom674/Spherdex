14d05e14c80b73251b83cd22cfcd3c57e6f26f8c
Terranom674 <thomas_dannenberg@web.de>
2025-01-24 10:46:00 +0100

    Delete spherdex directory

--- spherdex/mitgliederverwaltung/doctype/mitgliederrolle/mitgliederrolle.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Thomas Dannenberg and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class Mitgliederrolle(Document):
-	pass
