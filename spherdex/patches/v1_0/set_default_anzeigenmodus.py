557381198870697f7941f07e94b4f23736ae9465
Terranom674 <thomas_dannenberg@web.de>
2025-01-27 18:34:40 +0100

    Delete spherdex directory

--- spherdex/patches/v1_0/set_default_anzeigenmodus.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-def execute():
-    settings = frappe.get_single("Mitgliederverwaltung Einstellungen")
-    if not settings.default_anzeigenmodus:
-        settings.default_anzeigenmodus = "Checkbox"
-        settings.save()
-        frappe.db.commit()
